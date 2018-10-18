79a51a066c31d676d61f08f32149e97f59c7f161
caesar chan <caeasar_chan@live.com>
2018-10-18 14:34:48 +0800

    update domain

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,7 +1,7 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  SERVER_URL: '"http://mapi.actiview.com"',
-  HTTPS_SERVER_URL: '"https://mapi.actiview.com"',
+  SERVER_URL: '"http://mapi.xingstation.com"',
+  HTTPS_SERVER_URL: '"https://mapi.xingstation.com"',
   IMG_URL: '"http://p22vy0aug.bkt.clouddn.com/"'
 }
