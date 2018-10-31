6b4f0183b90be51df1b1fb5549b74babff3b1dd3
Joy <joywangx@sina.com>
2018-10-31 16:23:01 +0800

    fix

--- src/router/pc/coupon.js
+++ src/router/pc/coupon.js
@@ -27,50 +27,30 @@ let router = {
           },
           component: () =>
             import(/* webpackChunkName: "page/coupon/verification/index" */ 'page/coupon/verification/index')
-        },
+        }
       ]
     },
-    // {
-    //   path: 'van',
-    //   name: '核销账号',
-    //   redirect: 'van',
-    //   meta: {
-    //     title: '核销账号'
-    //   },
-    //   component: () =>
-    //     import(/* webpackChunkName: "page/coupon/verifyAccount/routerView" */ 'page/verify/verifyAccount/routerView'),
-    //   children: [
-    //     {
-    //       path: '/',
-    //       name: '核销账号列表',
-    //       meta: {
-    //         title: '核销账号列表'
-    //       },
-    //       component: () =>
-    //         import(/* webpackChunkName: "page/verify/verifyAccount/index" */ 'page/verify/verifyAccount/index')
-    //     },
-    //     {
-    //       path: 'add',
-    //       name: '新建核销账号',
-    //       meta: {
-    //         title: '新建核销账号',
-    //         permission: ''
-    //       },
-    //       component: () =>
-    //         import(/* webpackChunkName: "page/verify/verifyAccount/save" */ 'page/verify/verifyAccount/save')
-    //     },
-    //     {
-    //       path: 'edit/:uid',
-    //       name: '编辑核销账号',
-    //       meta: {
-    //         title: '编辑核销账号告',
-    //         permission: ''
-    //       },
-    //       component: () =>
-    //         import(/* webpackChunkName: "page/verify/verifyAccount/save" */ 'page/verify/verifyAccount/save')
-    //     }
-    //   ]
-    // }
+    {
+      path: 'rules',
+      name: '优惠券规则',
+      redirect: 'rules',
+      meta: {
+        title: '优惠券规则'
+      },
+      component: () =>
+        import(/* webpackChunkName: "page/coupon/rules/routerView" */ 'page/coupon/rules/routerView'),
+      children: [
+        {
+          path: '/',
+          name: '优惠券规则列表',
+          meta: {
+            title: '优惠券规则列表'
+          },
+          component: () =>
+            import(/* webpackChunkName: "page/coupon/rules/index" */ 'page/coupon/rules/index')
+        }
+      ]
+    }
   ]
 }
 
