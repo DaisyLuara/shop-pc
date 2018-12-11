389a26d7802dd9b8310b2c7b39f5015c0ac9a874
Joy <joy@fdvdeMacBook-Pro.local>
2018-12-11 19:53:36 +0800

    fix

--- src/page/coupon/verification/index.vue
+++ src/page/coupon/verification/index.vue
@@ -134,7 +134,7 @@
                   <span>{{ scope.row.use_date }}</span>
                 </el-form-item>
                 <el-form-item label="核销时间">
-                  <span>{{ scope.row.effect_start_date ? (scope.row.effect_start_date +'-'+ scope.row.effect_end_date) : '' }}</span>
+                  <span>{{ scope.row.effect_start_date ? (scope.row.effect_start_date +' -- '+ scope.row.effect_end_date) : '' }}</span>
                 </el-form-item>
                 <el-form-item label="核销人">
                   <span>{{ scope.row.customer !== undefined ? scope.row.customer.name : '' }}</span>
