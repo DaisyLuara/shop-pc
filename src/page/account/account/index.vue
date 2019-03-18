d2ac790be39d53ad5a9ee27f720b139c6dda9b4c
daisyluara <1481795291@qq.com>
2019-03-18 10:45:15 +0800

    css1

--- src/page/account/account/index.vue
+++ src/page/account/account/index.vue
@@ -8,38 +8,49 @@
     <div class="account-wrap">
       <!-- 商户信息 -->
       <div class="account-details">
-        <!-- <div 
-          class="account-avatar">
-          <img 
-            :src="avatar" 
-            class="avatar">
-        </div>-->
         <div class="account-content">
-          <div v-show="!editName" class="account-item">
+          <div
+            v-show="!editName"
+            class="account-item"
+          >
             <div class="account-item-title">
-              公司名称：
+              公司名称
+            </div>
+            <div class="account-item-content">{{ name }}
               <i
                 class="el-icon-edit-outline icon-edit"
                 @click="editName = true, company_name = name"
               />
             </div>
-            <div class="account-item-content">{{ name }}</div>
           </div>
-          <div v-show="editName" class="account-item">
+          <div
+            v-show="editName"
+            class="account-item"
+          >
             <div class="account-item-title">公司名称：</div>
-            <el-input v-model="company_name" class="item-input"/>
-            <el-button type="primary" size="mini" @click="saveCompanyName">保存</el-button>
+            <el-input
+              v-model="company_name"
+              class="item-input"
+            />
+            <el-button
+              type="primary"
+              size="mini"
+              @click="saveCompanyName"
+            >保存</el-button>
           </div>
           <div class="account-item">
             <div class="account-item-title">认证等级:</div>
           </div>
           <div class="account-item grade">
             <span>
-              <img :src="IMG_URL+'ad_shop/img/account_2.png?v=1'" class="grade_1">
+              <img
+                :src="IMG_URL+'ad_shop/img/account_2.png?v=1'"
+                class="grade_1"
+              >
             </span>
             <span class="grade_name">LV.1</span>
             <div class="grade-line">
-              <div class="grade-line-done"/>
+              <div class="grade-line-done" />
             </div>
           </div>
         </div>
@@ -49,25 +60,31 @@
         <div class="account-title">
           <h4>账号设置</h4>
           <div class="grade-wrap">
-            <span class="grade-column"/>
-            <span class="grade-column"/>
-            <span class="grade-column"/>
-            <span class="grade-column one"/>
-            <span class="grade-column two"/>
-            <span class="grade-column three"/>
+            <span class="grade-column" />
+            <span class="grade-column" />
+            <span class="grade-column" />
+            <span class="grade-column one" />
+            <span class="grade-column two" />
+            <span class="grade-column three" />
           </div>
           <div class="account-safety">安全评分80分,账户存在安全风险,可进一步提升</div>
         </div>
-        <div v-show="!formShow" class="account-password-warp">
+        <div
+          v-show="!formShow"
+          class="account-password-warp"
+        >
           <span>
-            <img :src="IMG_URL+'ad_shop/img/lock.png?v=2'" class="lock">
+            <img
+              :src="IMG_URL+'ad_shop/img/lock.png?v=2'"
+              class="lock"
+            >
           </span>
           <div class="password-grade-wrap">
             <div class="grade-title">密码强度（高）</div>
             <div class="grade-info">目前您的密码强度高</div>
           </div>
           <el-button @click="modifyPassword">修改密码</el-button>
-        </div>
+        </div> -->
         <!-- 修改密码 -->
         <el-form
           v-show="formShow"
@@ -78,7 +95,10 @@
           label-position="right"
           label-width="80px"
         >
-          <el-form-item label="新密码" prop="newPass">
+          <el-form-item
+            label="新密码"
+            prop="newPass"
+          >
             <el-input
               v-model="passwordForm.newPass"
               type="password"
@@ -86,7 +106,10 @@
               class="item-input"
             />
           </el-form-item>
-          <el-form-item label="确认密码" prop="checkPass">
+          <el-form-item
+            label="确认密码"
+            prop="checkPass"
+          >
             <el-input
               v-model="passwordForm.checkPass"
               type="password"
@@ -95,76 +118,16 @@
             />
           </el-form-item>
           <el-form-item>
-            <el-button type="primary" @click="submitForm('passwordForm')">提交</el-button>
+            <el-button
+              type="primary"
+              @click="submitForm('passwordForm')"
+            >提交</el-button>
             <el-button @click="resetForm('passwordForm')">取消</el-button>
           </el-form-item>
         </el-form>
       </div>
     </div>
-    <!-- 等级弹出框 -->
-    <el-dialog :visible.sync="gradeVisible" :fullscreen="true" title="等级特权信息">
-      <el-table :data="gridData">
-        <el-table-column prop="grade" label="认证等级"/>
-        <el-table-column prop="place" label="授权场地数"/>
-        <el-table-column prop="singleNum" label="每个场地授权押金"/>
-        <el-table-column prop="sum" label="押金总金额"/>
-        <el-table-column prop="hiNum" label="每月获赠嗨豆"/>
-        <el-table-column prop="ad" label="线下同事投放广告数"/>
-      </el-table>
-    </el-dialog>
-    <!-- 充值弹出框 -->
-    <el-dialog :visible.sync="payVisible" :fullscreen="true" title="万达公司 (余额：1,000通用快乐币)">
-      <div class="pay-content">
-        <div class="pay-title">充值项目</div>
-        <div class="general-pay">
-          <div class="pay-item">
-            <div class="hi_num">10 嗨币</div>
-            <div class="money_num">¥ 10.00</div>
-          </div>
-          <div class="pay-item">
-            <div class="hi_num">100 嗨币</div>
-            <div class="money_num">¥ 100.00</div>
-            <div class="pay-hot">HOT</div>
-          </div>
-          <div class="pay-item">
-            <div class="hi_num">500 嗨币</div>
-            <div class="money_num">¥ 500.00</div>
-            <div class="pay-hot">HOT</div>
-          </div>
-          <div class="pay-item">
-            <div class="hi_num">1000 嗨币</div>
-            <div class="money_num">¥ 1000.00</div>
-          </div>
-        </div>
-        <div class="pay-title">其他充值</div>
-        <el-input v-model="payValue" class="item-input"/>
-      </div>
-      <div class="pay-method-wrap">
-        <div class="pay-title">支付方式</div>
-        <div class="pay-content">
-          <el-tabs tab-position="left" class="tab-wrap">
-            <el-tab-pane>
-              <span slot="label">
-                支付宝
-                <img :src="IMG_URL+'ad_shop/img/alipay.png'" style="width: 20%;">
-              </span>
-              <div class="qr-code">
-                <img :src="IMG_URL+'ad_shop/img/test.png'">
-              </div>
-            </el-tab-pane>
-            <el-tab-pane>
-              <span slot="label">
-                微信
-                <img :src="IMG_URL+'ad_shop/img/wechat.png'" style="width: 20%;">
-              </span>
-              <div class="qr-code">
-                <img :src="IMG_URL+'ad_shop/img/test1.png'">
-              </div>
-            </el-tab-pane>
-          </el-tabs>
-        </div>
-      </div>
-    </el-dialog>
+
   </div>
 </template>
 
@@ -369,12 +332,9 @@ export default {
 </script>
 <style scoped lang="less">
 .root {
-  // padding: 10px;
   font-size: 14px;
-  color: #5e6d82;
+  color: #7f58cd;
   .account-wrap {
-    background: #fff;
-    padding: 30px;
     .item-input {
       width: 250px;
     }
@@ -395,7 +355,7 @@ export default {
           color: #444;
           font-size: 18px;
           .account-item-title {
-            color: #9c9fa2;
+            color: #7f58cd;
             font-size: 14px;
             margin-bottom: 10px;
           }
@@ -404,7 +364,7 @@ export default {
             font-size: 16px;
           }
           .icon-edit {
-            color: #20a0ff;
+            color: #7f58cd;
             cursor: pointer;
             font-size: 16px;
             font-weight: 600;
@@ -415,24 +375,24 @@ export default {
             width: 6%;
           }
           .grade_name {
-            color: #20a0ff;
+            color: #7f58cd;
           }
           .grade-line {
             width: 250px;
             height: 4px;
             display: inline-block;
-            background: #ededed;
+            background: #7f58cd;
             position: relative;
             .grade-line-done {
               position: absolute;
               width: 80%;
               height: 4px;
               display: inline-block;
-              background: #20a0ff;
+              background: #7f58cd;
             }
           }
           .grade_text {
-            color: #1988cb;
+            color: #7f58cd;
             font-size: 13px;
             cursor: pointer;
             margin-left: 10px;
@@ -538,80 +498,7 @@ export default {
           }
           .grade-info {
             font-size: 14px;
-            color: #95a7b7;
-          }
-        }
-      }
-    }
-  }
-  .pay-content {
-    .item-input {
-      width: 250px;
-    }
-    .pay-title {
-      font-size: 16px;
-      color: #444;
-      font-weight: 600;
-      margin-bottom: 20px;
-    }
-    .general-pay {
-      display: flex;
-      flex-direction: row;
-      justify-content: flex-start;
-      margin-bottom: 15px;
-      .pay-item {
-        border: 1px solid #c0ccda;
-        border-radius: 4px;
-        background: #fff;
-        padding: 10px 30px;
-        text-align: center;
-        cursor: pointer;
-        margin-right: 20px;
-        position: relative;
-        .pay-hot {
-          width: 29px;
-          height: 14px;
-          background-color: #ff7a7a;
-          color: #fff;
-          font-size: 10px;
-          line-height: 14px;
-          text-align: center;
-          position: absolute;
-          right: -1px;
-          top: -1px;
-          border-top-right-radius: 5px;
-        }
-        .hi_num {
-          font-size: 18px;
-          color: #f56c6c;
-          margin-bottom: 5px;
-        }
-        .money_num {
-          color: #c0ccda;
-          font-size: 14px;
-        }
-      }
-    }
-  }
-  .pay-method-wrap {
-    margin-top: 15px;
-    .pay-title {
-      font-size: 16px;
-      color: #444;
-      font-weight: 600;
-      margin-bottom: 20px;
-    }
-    .pay-content {
-      height: 245px;
-      border: 1px solid #c0ccda;
-      border-radius: 4px;
-      padding: 10px;
-      .tab-wrap {
-        height: 100%;
-        .qr-code {
-          text-align: center;
-          img {
-            width: 22%;
+            color: #7f58cd;
           }
         }
       }
