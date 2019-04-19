fceddb5877c4ccc49bdc421c1c3817ba0a36360e
daisyluara <1481795291@qq.com>
2019-04-19 19:17:55 +0800

    dev config

--- config/dev.env.js
+++ config/dev.env.js
@@ -3,7 +3,7 @@ var prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  SERVER_URL: '"http://mapi.jinfree.top"',
-  HTTPS_SERVER_URL: '"http://mapi.jinfree.top"',
+  SERVER_URL: '"http://mapi.jingfree.top"',
+  HTTPS_SERVER_URL: '"http://mapi.jingfree.top"',
   IMG_URL: '"http://cdn.xingstation.cn/"'
 })
